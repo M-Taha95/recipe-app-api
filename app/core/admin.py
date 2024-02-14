8ca3ca3301c7d0e1ff4a87869dda229a6c3f831d
M-Taha95echo <msoudi2013@gmail.com>
2024-02-14 21:56:28 +0300

    Setup Admin Django

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ["email", "name"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_('Personal Info'), {'fields': ('name',)}),
+        (_("Personal Info"), {"fields": ("name",)}),
         (
             _("Permissions"),
             {
@@ -24,11 +24,28 @@ class UserAdmin(BaseUserAdmin):
                     "is_staff",
                     "is_superuser",
                 )
-            }
+            },
         ),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 # admin.site.register(models.User)
